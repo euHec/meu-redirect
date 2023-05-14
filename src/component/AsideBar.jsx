13543cb005afee961f0f1bf28349e967865cd3b2
euHec <hectorsouzass@gmail.com>
2023-05-14 14:32:27 -0300

    style: concluir estilização mobile da aplicação

--- src/component/AsideBar.jsx
+++ src/component/AsideBar.jsx
@@ -1,14 +1,38 @@
 import { useContext } from "react";
 import { context } from "../context/AppProvider";
+import { useHistory } from "react-router-dom";
 
 export default function AsideBar() {
-  const { classType } = useContext(context);
+  const { classType, setClassType } = useContext(context);
+
+  const { push } = useHistory();
 
   return (
-    <div className={ `aside-${classType}` }>
-      <p>Simulador</p>
-      <p>Carrinho</p>
-      <p>Sair</p>
+    <div className={ `aside aside-${classType}` }>
+      <p onClick={ () => {
+          push('/product')
+          setClassType(!classType)
+        }
+      }
+        >Simulador
+      </p>
+      <p onClick={
+        () => {
+          push('/cart')
+          setClassType(!classType)
+        }
+      }
+      >
+        Carrinho</p>
+      <p onClick={
+        () => {
+          push('/')
+          setClassType(!classType)
+        } 
+      }
+      >
+        Sair
+      </p>
     </div>
   );
 }
