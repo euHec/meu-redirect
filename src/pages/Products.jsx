91c816c9c520f805988d2053ad33e6247b3c9a4d
euHec <hectorsouzass@gmail.com>
2023-05-14 14:55:20 -0300

    Feat: Implementa calculadora em tempo real do produto

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,7 +8,7 @@ import AsideBar from '../component/AsideBar';
 
 export default function Products() {
   const {
-    setListProduct
+    setListProduct, price, impost
   } = useContext(context)
 
   useEffect(() => {
@@ -16,6 +16,7 @@ export default function Products() {
     setListProduct(list)
   }, [setListProduct])
 
+  const parcialPrice = impost === '6.5' ? (Number(price) + Number(price) * (Number(impost) / 100)) * 5.30 : Number(price) * 5.30;
 
   return(
     <>
@@ -23,6 +24,7 @@ export default function Products() {
       <AsideBar />
       <main>
         <h1>Simule o seu pedido!</h1>
+        <h1>{ `R$ ${parcialPrice.toFixed(2)}` }</h1>
         <Form />
       </main>
     </>
